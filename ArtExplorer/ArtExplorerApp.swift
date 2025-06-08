66b7172f69fd277e25dda423cb6aa6fc4a48d922
Rafael Almeida <apprafael.almeida@gmail.com>
2025-06-08 10:11:27 -0300

    Sets up initial project structure
    Initializes the Xcode project with basic configurations,
    app structure, assets, and an initial content view.
    
    Adds necessary files for a basic SwiftUI application, including
    asset catalogs, an app delegate, and a content view.
    
    Configures project settings for both Debug and Release builds.

---
+++ ArtExplorer/ArtExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtExplorerApp.swift
+//  ArtExplorer
+//
+//  Created by Rafael Almeida on 08/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
