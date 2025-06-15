67281d5b6b7e5a969695a9cb5de8e03ec42e2a85
Thalisson Gabriel da Silva Santos Melo <thalisson.melo@pxbank.com.br>
2025-06-15 17:09:31 -0300

    Initial Commit

---
+++ ArtExplorer/ArtExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtExplorerApp.swift
+//  ArtExplorer
+//
+//  Created by Thalisson Melo on 15/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtExplorerTests/ArtExplorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtExplorerTests.swift
+//  ArtExplorerTests
+//
+//  Created by Thalisson Melo on 15/06/25.
+//
+
+import Testing
+@testable import ArtExplorer
+
+struct ArtExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
